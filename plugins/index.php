6ada4f39e8bd0c1e05ffd339e85c2fa933d4b7cd
Zyadliftawi <ki@lb-h.com>
2013-01-24 02:56:07 +0300

    Update plugins/index.php

--- plugins/index.php
+++ plugins/index.php
@@ -1,3 +1,31 @@
-<?php
-// Silence is golden.
-?>
+<html dir="rtl">
+
+<head>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>~! Palestine &amp; Algeria ~! </title>
+</head>
+
+<body link="#00FFFF" text="#FF0000" bgcolor="#000000">
+
+<p align="center"><font face="BoisterBlack" size="7">Palestine Algeria Team
+</font></p>
+<table border="1" width="100%" id="table1">
+  <tr>
+		<td>Anas Alshekh </td>
+		<td>Ana Mr Hell</td>
+		<td>Mr.Zyad Liftawi</td>
+	</tr>
+	<tr>
+		<td><a href="mailto:ajp@london.com">ajp@london.com</a></td>
+		<td><font color="#00FFFF">amr@hacker.ps</font></td>
+		<td><a href="mailto:zml@london.com">zml@london.com</a></td>
+	</tr>
+</table>
+
+<a href="http://www.uploadmusic.org"><object type="application/x-shockwave-flash" width="17"
+height="17"data="http://www.uploadmusic.org/musicplayer.swf?song_url=http://www.uploadmusic.org/MUSIC/7770361358720230.mp3&autoplay=true"><param  name="movie"value="http://www.uploadmusic.org/musicplayer.swf?song_url=http://www.uploadmusic.org/MUSIC/7770361358720230.mp3&song_title=uploadmusic.org&autoplay=true"
+/></object><img src="http://www.uploadmusic.org/smallplayer.gif" border="0">
+<br>Upload Music</a></body>
+
+</html>
